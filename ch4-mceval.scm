4d2a5802b9d6d1a0e8793f12dd39556bc1584fbd
Eric Woo <etwoo@github>
2017-08-25 23:05:46 -0700

    exercise 4.1: evaluation order

--- ch4-mceval.scm
+++ ch4-mceval.scm
@@ -59,6 +59,22 @@
       (cons (eval (first-operand exps) env)
             (list-of-values (rest-operands exps) env))))
 
+;;;EXERCISE 4.1 -- BEGIN
+
+(define (list-of-values-left-to-right exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((left (eval (first-operand exps) env)))
+        (cons left (list-of-values (rest-operands exps) env)))))
+
+(define (list-of-values-right-to-left exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((right (list-of-values (rest-operands exps) env)))
+        (cons (eval (first-operand exps) env) right))))
+
+;;;EXERCISE 4.1 -- END
+
 (define (eval-if exp env)
   (if (true? (eval (if-predicate exp) env))
       (eval (if-consequent exp) env)
